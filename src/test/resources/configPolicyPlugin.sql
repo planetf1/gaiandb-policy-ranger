f5523b64b3b51d0c564b4119a423cb83a2ff4b47
planetf1 <nigel@cherrybyte.me.uk>
2017-04-25 10:04:23 +0100

    Added simple test script to update gaiandb config

---
+++ src/test/resources/configPolicyPlugin.sql
@@ -0,0 +1,2 @@
+call setconfigproperty('SQL_QUERY_FILTER', 'com.ibm.gaiandb.policy.RangerPolicyQueryFilter')
+call setconfigproperty('SQL_RESULTS_FILTER', 'com.ibm.gaiandb.policy.RangerPolicyResultFilter')
